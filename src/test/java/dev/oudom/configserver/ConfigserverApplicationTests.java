d71a4bc9895c65c64310c8a234fb821db2f6fb0f
oudom <oudomphoem@gmail.com>
2026-01-05 12:38:47 +0700

    init config server

---
+++ src/test/java/dev/oudom/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oudom.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
